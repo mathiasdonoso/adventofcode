bfb665f0d32a4123293e02a46ceab8332fd0c369
Mathías Donoso <mathiasd88@gmail.com>
2019-01-24 16:41:02 -0300

    day 1 - part 1

--- day1.go
+++ day1.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+)
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	data, err := ioutil.ReadFile("day1.txt")
+	handleError(err)
+	str := string(data)
+	fmt.Println(InverseCaptcha(str))
+}
+
+// InverseCaptcha Problem Day 1
+// The captcha requires you to review a sequence of digits (your puzzle input)
+// and find the sum of all digits that match the next digit in the list.
+// The list is circular, so the digit after the last digit is the first digit in the list.
+// https://adventofcode.com/2017/day/1
+func InverseCaptcha(numbers string) int {
+	sum := 0
+
+	for pos := range numbers {
+		var next string
+		var nextInt int
+		actual := string(numbers[pos])
+		actualInt, _ := strconv.Atoi(actual)
+		if pos < len(numbers)-1 {
+			next = string(numbers[pos+1])
+		} else {
+			next = string(numbers[0])
+		}
+
+		nextInt, _ = strconv.Atoi(next)
+
+		if actualInt == nextInt {
+			sum = sum + actualInt
+		}
+	}
+
+	return sum
+}
--- day1_test.go
+++ day1_test.go
@@ -1,1 +1,23 @@
 package main
+
+import "testing"
+
+func assertEquals(t *testing.T, spected interface{}, returned interface{}) {
+	if spected != returned {
+		t.Fatal("It should be", spected, "but instead it returns", returned)
+	}
+}
+
+func TestInverseCaptcha(t *testing.T) {
+	test1 := InverseCaptcha("1122")
+	assertEquals(t, 3, test1)
+
+	test2 := InverseCaptcha("1111")
+	assertEquals(t, 4, test2)
+
+	test3 := InverseCaptcha("1234")
+	assertEquals(t, 0, test3)
+
+	test4 := InverseCaptcha("91212129")
+	assertEquals(t, 9, test4)
+}
